83777c8c4feffee53267c7f9820b1796edd6ad47
Vitor <vtloboss@hotmail.com>
2021-08-25 21:36:58 -0300

    Configurado variaveis de AWS e otimizado upload com CollectFast

--- pypro/settings.py
+++ pypro/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'collectfast'
+    'collectfast',
     'django.contrib.staticfiles',
 ]
 
@@ -153,10 +153,11 @@ if AWS_ACCESS_KEY_ID:
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
+    AWS_S3_CUSTOM_DOMAIN = None
 
     COLLECTFAST_ENABLE = True
 
-    AWS_DEFAULT_ACL = None
+    AWS_DEFAULT_ACL = 'private'
 
 # static assets
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
