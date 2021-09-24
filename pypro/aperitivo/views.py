50e823f53d44607ff5d49d7b26f6e94a78833c8c
Vitor <vtloboss@hotmail.com>
2021-09-24 16:15:19 -0300

    Aplicando filtro nos titulos!

--- pypro/aperitivo/views.py
+++ pypro/aperitivo/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    videos = {'motivacao':{ 'titulo': 'Video aperitivo : Motivação', 'vimeo_id': '602764344?h=963b6c3f44'},
-    'instalacao-windows': {'titulo': 'Instalação no Windows', 'vimeo_id': '251497668'},
+    videos = {'motivacao': {'titulo': 'Video aperitivo : Motivação', 'vimeo_id': '602764344?h=963b6c3f44'},
+              'instalacao-windows': {'titulo': 'Instalação no Windows', 'vimeo_id': '251497668'},
               }
     video = videos[slug]
     return render(request, 'aperitivo/video.html', context={'video': video})
