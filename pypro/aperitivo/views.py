df02edc7a972c2bdbe1ce8083e98acab9b625a69
Vitor <vtloboss@hotmail.com>
2021-09-23 23:19:24 -0300

    Videos de acordo com o slug!

--- pypro/aperitivo/views.py
+++ pypro/aperitivo/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivo/video.html')
+    videos = {'motivacao':{ 'titulo': 'Video aperitivo : Motivação', 'vimeo_id': '602764344?h=963b6c3f44'},
+    'instalacao-windows': {'titulo': 'Instalação-Windows', 'vimeo_id': '251497668'},
+              }
+    video = videos[slug]
+    return render(request, 'aperitivo/video.html', context={'video': video})
